8a27823dba9d075d971faf049f80706d79e5a9d2
wjx-wp <jp.wang04@gmail.com>
2026-08-13 13:25:17 +0800

    feat(documentation): add dedicated documentation capture control

---
+++ desktop/src/components/control-bar/document-capture/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <slot v-bind="{ loading }" :trigger="() => captureOriginal(device)" />
+</template>
+
+<script setup>
+defineOptions({ inheritAttrs: false })
+
+const props = defineProps({
+  device: {
+    type: Object,
+    default: () => ({}),
+  },
+})
+
+const { loading, captureOriginal } = useDocumentationAction()
+</script>
