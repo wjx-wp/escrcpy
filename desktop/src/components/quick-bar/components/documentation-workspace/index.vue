cf3d94af995b2d959928692f73bee848ad7aa9e3
wjx-wp <jp.wang04@gmail.com>
2026-08-13 15:15:53 +0800

    feat(documentation): add workspace chooser quick action

---
+++ desktop/src/components/quick-bar/components/documentation-workspace/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <slot v-bind="{ loading }" :trigger="chooseWorkspace" />
+</template>
+
+<script setup>
+const { loading, chooseWorkspaceRoot } = useDocumentationAction()
+
+async function chooseWorkspace() {
+  await chooseWorkspaceRoot()
+}
+</script>
