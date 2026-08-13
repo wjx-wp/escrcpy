5e6c1ef0f5d1d3af3d880de550fe533cd5719bb0
wjx-wp <jp.wang04@gmail.com>
2026-08-13 18:17:14 +0800

    fix(guidepix): avoid trayPath export collision

--- desktop/electron/configs/logo/index.js
+++ desktop/electron/configs/logo/index.js
@@ -1,7 +1,6 @@
 import { buildResolve } from '$electron/process/resources.js'
 
 export const logoPath = buildResolve('logo.svg')
-export const trayPath = logoPath
 
 export function getLogoPath() {
   return logoPath
